e94db5baf76c7f7e47bfe7f6806b2ad2ea80fc0c
antihax <antihax@gmail.com>
2019-08-04 20:17:57 -0700

    Simplify

--- cmd/sapexport/suim.go
+++ cmd/sapexport/suim.go
@@ -11,24 +11,12 @@ import (
 
 func init() {
 	var file string
-	/*
-		    - SUSR_SUIM_API_RSUSR002: Users by complex selection criteria
-		    - SUSR_SUIM_API_RSUSR008_009_NEW: Users with critical combinations of auhthorizations
-		    - SUSR_SUIM_API_RSUSR020: Profiles by complex selection criteria
-		    - SUSR_SUIM_API_RSUSR050_AUTH: Authorization Comparison
-		    - SUSR_SUIM_API_RSUSR050_PROF: Profile Comparison
-		    - SUSR_SUIM_API_RSUSR050_ROLE: Role Comparison
-		    - SUSR_SUIM_API_RSUSR050_USER: User Comparison
-		    - SUSR_SUIM_API_RSUSR070: Roles by complex search criteria
-		    - SUSR_SUIM_API_RSUSR100N: Change documents for users
-			- SUSR_SUIM_API_RSUSR200: Users according to logon date and password change
-	*/
 
-	var cmdChangeDocs = &cobra.Command{
-		Use:   "changedocs [user]",
-		Short: "Search changedocs",
-		Long:  `changedocs will list change documents that match the parameters.`,
-		Args:  cobra.MinimumNArgs(0),
+	var cmdRun = &cobra.Command{
+		Use:   "run",
+		Short: "run function module",
+		Long:  `run a function module over RFC.`,
+		Args:  cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			s := sap.RFC{}
 
@@ -42,20 +30,9 @@ func init() {
 				if err != nil {
 					log.Fatalln(err)
 				}
-			} else {
-				params = map[string]interface{}{
-					"IT_USER": []map[string]interface{}{
-						map[string]interface{}{
-							"SIGN": "I",
-							"LOW":  args[0],
-						}},
-					"IV_PASS": true,
-					"IV_ROLE": true,
-					"IV_PROF": true,
-				}
 			}
 
-			result, err := s.Call("SUSR_SUIM_API_RSUSR100N", params)
+			result, err := s.Call(args[0], params)
 			if err != nil {
 				log.Fatalln(err)
 			}
@@ -75,56 +52,7 @@ func init() {
 		},
 	}
 
-	rootCmd.AddCommand(cmdChangeDocs)
-	cmdChangeDocs.Flags().StringVarP(&file, "file", "f", "", "YAML file containing query parameters")
-
-	var cmdUsers = &cobra.Command{
-		Use:   "users [user]",
-		Short: "Search users",
-		Long:  `users will list users that match the parameters.`,
-		Args:  cobra.MinimumNArgs(0),
-		Run: func(cmd *cobra.Command, args []string) {
-			s := sap.RFC{}
-
-			err := s.Connect(abapSystem)
-			if err != nil {
-				log.Fatalln(err)
-			}
-
-			var params map[string]interface{}
-			if file != "" {
-				params, err = readYaml(file)
-				if err != nil {
-					log.Fatalln(err)
-				}
-			} else {
-				params = map[string]interface{}{
-					"IT_USER": []map[string]interface{}{map[string]interface{}{
-						"SIGN": "I",
-						"LOW":  args[0],
-					}},
-				}
-			}
-
-			result, err := s.Call("SUSR_SUIM_API_RSUSR002", params)
-			if err != nil {
-				log.Fatalln(err)
-			}
-
-			users, ok := result["ET_USERS"]
-			if !ok {
-				log.Fatalf("Found no users: %v\n", result["RETURN"])
-			}
-
-			j, err := json.MarshalIndent(users, "", "  ")
-			if err != nil {
-				log.Fatalln(err)
-			}
-			fmt.Printf("%s\n", j)
-		},
-	}
-
-	rootCmd.AddCommand(cmdUsers)
-	cmdUsers.Flags().StringVarP(&file, "file", "f", "", "YAML file containing query parameters")
+	rootCmd.AddCommand(cmdRun)
+	cmdRun.Flags().StringVarP(&file, "file", "f", "", "YAML file containing query parameters")
 
 }
